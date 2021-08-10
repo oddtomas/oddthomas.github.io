0643270c312108820b896523376ad76c79950255
oddtomas <oddthomas@csu.fullerton.edu>
2021-08-09 21:13:38 -0700

    updated react routers with nav

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
-import Navbar from './components/Navbar.js'
+// import Navbar from './components/Navbar.js'
 import './App.css';
 import background from "../src/pictures/background.jpg"
 import github from '../src/pictures/github.png'; 
 import linkedin from '../src/pictures/linkedin.png'; 
+import {Route, BrowserRouter as Router, Link} from "react-router-dom";
+import About from "./pages/About"
+import Home from "./pages/Home"
+import Projects from "./pages/Projects"
 
 
 
@@ -17,6 +21,7 @@ this.state = {
   }
   render () {
   return (
+    <Router>
     <>
     <div className="App" style={{ backgroundImage: `url(${background})` }}
     >
@@ -26,9 +31,25 @@ this.state = {
       
         <a href="https://www.linkedin.com/in/thomas-nguyen-9665761a7/"> <img src={linkedin} alt="linkedin" width="50" height="50" /></a>
     </p>
-      <h1 id="head">DEV.</h1>
+      <nav>
+          <ul>
+            
+              <Link to="/">home</Link>
+              
+             <p> <Link to="/about">About</Link> </p>
+             <p> <Link to="/projects">Projects</Link> </p>
+
+              
+          </ul>
+      </nav>
+      <div>
+      <Route path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/projects" component={Projects} />
+      </div>
 </div>
 </>
+</Router>
   )
   }
 }
