f6497a6fc7aeacd485c150bfc098d77e946c16de
oddtomas <oddthomas@csu.fullerton.edu>
2021-08-15 20:58:19 -0700

    fixed whitespace overscroll issue

--- src/App.js
+++ src/App.js
@@ -1,70 +1,76 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 // import Navbar from './components/Navbar.js'
-import './App.css';
-import background from "../src/pictures/background.jpg"
-import github from '../src/pictures/github.png'; 
-import linkedin from '../src/pictures/linkedin.png'; 
-import {Route, BrowserRouter as Router, Link} from "react-router-dom"; //has to be saved?
-import About from "./pages/About"
-import Home from "./pages/Home"
-import Projects from "./pages/Projects"
-import Footer from './components/footer';
-
-
+import "./App.css";
+import github from "../src/pictures/github.png";
+import linkedin from "../src/pictures/linkedin.png";
+import canvas from "../src/pictures/canvas.jpg";
+import { Route, BrowserRouter as Router, Link } from "react-router-dom"; //has to be saved?
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+// import Footer from "./components/footer";
 
 class App extends Component {
-  constructor (props){
-    super(props)
-this.state = {
-
-}
-
+  constructor(props) {
+    super(props);
+    this.state = {};
   }
 
-githubClick = (props) => {
-    window.open("https://github.com/oddtomas")
-  }
-linkedInClick = (props) => {
-    window.open("https://www.linkedin.com/in/thomas-nguyen-9665761a7/")
-  }
+  githubClick = (props) => {
+    window.open("https://github.com/oddtomas");
+  };
+  linkedInClick = (props) => {
+    window.open("https://www.linkedin.com/in/thomas-nguyen-9665761a7/");
+  };
 
-  render () {
-  return (
-    <Router>
-    <>
-    <div className="App" style={{ backgroundImage: `url(${background})` }}
-    >
-      <p id="links">
-      {/* left or middle mouse click takes you off page when creating new tab*/}
-      <a href="_self" onClick={this.githubClick} onMouseDown={this.githubClick} ><img src={github} alt="github" width="50" height="50" /></a>
-      
-        <a href="_self" onClick={this.linkedInClick} onMouseDown={this.linkedInClick}> <img src={linkedin} alt="linkedin" width="50" height="50" /></a>
-    </p>
-      <nav>
-          <ul>
-            
-              <Link to="/">home</Link>
-              
-             <p> <Link to="/about">About</Link> </p>
-             <p> <Link to="/projects">Projects</Link> </p>
-      
+  render() {
+    return (
+      <Router>
+        <>
+          <div className="App" style={{ backgroundImage: `url(${canvas})` }}>
+            <p id="links">
+              {/* left or middle mouse click takes you off page when creating new tab*/}
+              <a
+                href="_self"
+                onClick={this.githubClick}
+                onMouseDown={this.githubClick}
+              >
+                <img src={github} alt="github" width="50" height="50" />
+              </a>
 
+              <a
+                href="_self"
+                onClick={this.linkedInClick}
+                onMouseDown={this.linkedInClick}
+              >
+                {" "}
+                <img src={linkedin} alt="linkedin" width="50" height="50" />
+              </a>
+            </p>
+            <nav>
+              <ul>
+                <Link to="/">home</Link>
 
-              
-          </ul>
-      </nav>
-      <div>
-      <Route path="/" component={Home} />
-      <Route path="/about" component={About} />
-      <Route path="/projects" component={Projects} />
-      </div>
-</div>
-</>
-<Footer>
-</Footer>
-
-</Router>
-  )
+                <p>
+                  {" "}
+                  <Link to="/about">About</Link>{" "}
+                </p>
+                <p>
+                  {" "}
+                  <Link to="/projects">Projects</Link>{" "}
+                </p>
+              </ul>
+            </nav>
+            <div>
+              <Route path="/" component={Home} />
+              <Route path="/about" component={About} />
+              <Route path="/projects" component={Projects} />
+            </div>
+          </div>
+        </>
+        {/* <Footer></Footer> */}
+      </Router>
+    );
   }
 }
 
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,28 +1,23 @@
-import React, {Component} from 'react';  
+import React, { Component } from "react";
 
 class About extends Component {
-render (){
-return (
-
-<>
-<p class="test"> 
-Lorem Ipsum
-"Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
-"There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain..."
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur enim mauris, facilisis placerat euismod nec, aliquet non elit. Mauris mollis est neque, nec suscipit nibh cursus non. Morbi iaculis ut magna a malesuada. Suspendisse vitae diam eget felis pharetra lacinia a dictum est. Ut gravida luctus ullamcorper. Mauris et viverra lorem. Maecenas et placerat ligula, quis commodo turpis. Ut vel molestie nulla, sit amet pretium augue. Ut eu posuere felis. Curabitur et commodo mi, sit amet feugiat felis.
-
-Fusce malesuada vel purus fermentum dignissim. Quisque varius rutrum diam, vitae sodales metus dignissim non. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut tortor urna. Phasellus et erat mattis, vulputate nunc quis, pretium quam. Maecenas id cursus risus. Vivamus molestie quis diam eu tincidunt. Maecenas et commodo ante. Sed consectetur ligula eu arcu mattis mollis. Integer egestas faucibus egestas. Quisque pretium, orci in laoreet condimentum, nisl ex mollis est, ac pretium ex quam sit amet leo. Curabitur turpis risus, lacinia ac risus ac, aliquet mollis tellus. Donec non augue nisi. Sed vestibulum venenatis risus, auctor condimentum est mollis suscipit.
-
-Suspendisse potenti. Nulla commodo nisl velit, nec tristique neque ultrices non. Vestibulum et tortor mi. Nulla tincidunt, ex in tincidunt pellentesque, nibh ipsum ornare diam, eu pretium ipsum orci sed felis. Etiam ac augue quis ante blandit dapibus. Curabitur suscipit tincidunt augue eget gravida. Quisque ut tempor elit, eu porttitor ante. Suspendisse imperdiet mauris nisi, sed porta metus mattis vitae. Aliquam tellus diam, congue maximus dui sed, vehicula venenatis sapien. Phasellus urna tortor, varius et bibendum sit amet, aliquet eu tortor. Donec mattis augue eget orci hendrerit, eget interdum metus ullamcorper. In eget orci venenatis, luctus felis vitae, ullamcorper diam. Duis mattis, erat et mattis iaculis, enim eros interdum quam, eget volutpat massa arcu eu diam. Aenean dictum, tortor rutrum consequat ultrices, sapien neque ultrices lorem, eu maximus ex ante quis sem. Praesent venenatis blandit dui vel consectetur.
-
-Nam nisl sapien, commodo at leo in, auctor lobortis dui. Cras lorem nibh, faucibus a dolor vel, auctor posuere purus. Curabitur tellus dui, molestie sit amet lectus quis, varius ultrices felis. Ut finibus consectetur posuere. Sed pretium magna in urna volutpat pellentesque. Donec bibendum eget mi id lobortis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed imperdiet ipsum et aliquet facilisis. Nam aliquam elementum lorem id varius. Proin eleifend vulputate porta. Vestibulum a lobortis elit.
-
-Morbi ac augue nulla. Fusce semper ipsum sapien, et sagittis magna porta sit amet. Proin ullamcorper bibendum congue. Nulla consequat at magna non mollis. Morbi id libero ornare, rhoncus justo ac, lacinia quam. Nullam at aliquet lectus, vitae blandit nulla. Etiam vel dolor sit amet ligula mattis venenatis ut ut velit. Ut vitae ipsum dictum, pretium velit non, semper risus. In hac habitasse platea dictumst. Vestibulum vitae malesuada erat.</p>
-</>
-)
-
-
-}
+  render() {
+    return (
+      <>
+        <p class="test">
+          Hi I'm Thomas! I am a full stack software developer with a passion for
+          clean and efficient code. I also love making sleek and intuitive front
+          end designs. Reach me at: oddthomas@csu.fullerton.edu
+        </p>
+        {/* -who you are
+        - photo
+        -what kind of work I do
+        -specialized developer skills
+        -contact info
+        -how you want to brand yourself */}
+      </>
+    );
+  }
 }
 
-export default About
+export default About;
