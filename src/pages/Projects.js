82144f51789231fda1048c425528e176d7de417b
oddtomas <oddthomas@csu.fullerton.edu>
2021-09-14 21:35:41 -0700

    started on project display cards

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -8,70 +8,35 @@ import {
   CardDeck,
   CardSubtitle,
   CardBody,
+  CardLink,
+  CardImgOverlay,
 } from "reactstrap";
 
 const Projects = (props) => {
   return (
-    <CardDeck>
-      <Card>
-        <CardImg
-          top
-          width="100%"
-          src="/assets/256x186.svg"
-          alt="Card image cap"
-        />
-        <CardBody>
-          <CardTitle tag="h5">Card title</CardTitle>
-          <CardSubtitle tag="h6" className="mb-2 text-muted">
-            Card subtitle
-          </CardSubtitle>
-          <CardText>
-            This is a wider card with supporting text below as a natural lead-in
-            to additional content. This content is a little bit longer.
-          </CardText>
-          <Button>Button</Button>
-        </CardBody>
-      </Card>
-      <Card>
-        <CardImg
-          top
-          width="100%"
-          src="/assets/256x186.svg"
-          alt="Card image cap"
-        />
-        <CardBody>
-          <CardTitle tag="h5">Card title</CardTitle>
-          <CardSubtitle tag="h6" className="mb-2 text-muted">
-            Card subtitle
-          </CardSubtitle>
-          <CardText>
-            This card has supporting text below as a natural lead-in to
-            additional content.
-          </CardText>
-          <Button>Button</Button>
-        </CardBody>
-      </Card>
-      <Card>
-        <CardImg
-          top
-          width="100%"
-          src="/assets/256x186.svg"
-          alt="Card image cap"
-        />
-        <CardBody>
-          <CardTitle tag="h5">Card title</CardTitle>
-          <CardSubtitle tag="h6" className="mb-2 text-muted">
-            Card subtitle
-          </CardSubtitle>
-          <CardText>
-            This is a wider card with supporting text below as a natural lead-in
-            to additional content. This card has even longer content than the
-            first to show that equal height action.
-          </CardText>
-          <Button>Button</Button>
-        </CardBody>
-      </Card>
-    </CardDeck>
+    <>
+      {" "}
+      <div>
+        <Card>
+          <CardBody>
+            <CardTitle tag="h5">Card title</CardTitle>
+            <CardSubtitle tag="h6" className="mb-2 text-muted">
+              Card subtitle
+            </CardSubtitle>
+          </CardBody>
+          <img width="100%" src="/assets/318x180.svg" alt="Card image cap" />
+          <CardBody>
+            <CardText>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </CardText>
+            <CardLink href="#">Card Link</CardLink>
+            <CardLink href="#">Another Link</CardLink>
+          </CardBody>
+        </Card>
+        <br></br>
+      </div>
+    </>
   );
 };
 
