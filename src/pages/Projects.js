f1cdc72596a7ad7063ff1fb31a210a37a6625740
oddtomas <oddthomas@csu.fullerton.edu>
2022-08-31 17:30:42 -0700

    added a project

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Card, CardText, CardBody, CardTitle } from "reactstrap";
 import weebCoin from "../pictures/weebcoin.png";
+import bookGenerator from "../pictures/bookGenerator.png";
 
 const Projects = (props) => {
   return (
@@ -31,6 +32,31 @@ const Projects = (props) => {
               className="cardLink"
             >
               Code
+            </a>
+          </CardBody>
+        </Card>
+        <Card>
+          <CardBody>
+            <CardTitle tag="h5">SADA-U Book Generator</CardTitle>
+          </CardBody>
+          <img width="100%" src={bookGenerator} alt="book generator" />
+          <CardBody>
+            <CardText>
+              An AI text and image generator application that takes a users prompt, send its to a backend Flask server which feeds the prompt to the text and image generations models and uploads the images with the text attached as metadata to a google cloud storage bucket. Pubsub is used to listen for changes and trigger a callback on the front-end to look for the new items in the bucket and display them. Users can also download the finished book as a PDF.
+            </CardText>
+            <a
+              href="https://github.com/oddtomas/streamlit"
+              target="_blank"
+              className="cardLink"
+            >
+              Front-end Code
+            </a>
+            <a
+              href="https://github.com/ertrtx/backend-bookgenerator"
+              target="_blank"
+              className="cardLink"
+            >
+              Back-end Code
             </a>
           </CardBody>
         </Card>
